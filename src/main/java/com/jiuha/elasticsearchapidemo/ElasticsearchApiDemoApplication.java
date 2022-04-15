1264e8cc85c04d084b45444d5ff626c89e8ef0e6
jiuha <jiuhamashiro@sina.com>
2022-04-15 13:59:30 +0800

    项目上传测试

---
+++ src/main/java/com/jiuha/elasticsearchapidemo/ElasticsearchApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jiuha.elasticsearchapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApiDemoApplication.class, args);
+    }
+
+}
